3e5cc7f2e99b0b51df660adbaef1208b401a6c4c
Thiago Coelho <thiagovscoelho@gmail.com>
2023-03-17 08:24:08 -0300

    Changed hotkey and error message

--- RBTray.cpp
+++ RBTray.cpp
@@ -376,9 +376,9 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*
 
     WM_TASKBAR_CREATED = RegisterWindowMessage(L"TaskbarCreated");
 
-    BOOL registeredHotKey = RegisterHotKey(_hwndHook, 0, MOD_ALT | MOD_CONTROL, VK_DOWN);
+    BOOL registeredHotKey = RegisterHotKey(_hwndHook, 0, MOD_ALT | MOD_SHIFT, VK_DOWN);
     if (!registeredHotKey) {
-        MessageBox(NULL, L"Couldn't register hotkey", L"RBTray", MB_OK | MB_ICONERROR);
+        MessageBox(NULL, L"Couldn't register hotkey (Alt+Shift+Down)", L"RBTray", MB_OK | MB_ICONERROR);
     }
 
     MSG msg;
